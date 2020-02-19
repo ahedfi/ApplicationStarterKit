5e4b0b7dcbfc0afdb7cef5a9e3ea13ecca935afa
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2020-02-20 00:03:40 +0100

    add Address entity

--- Ahedfi.Server.Core.Domain/Entities/Customer.cs
+++ Ahedfi.Server.Core.Domain/Entities/Customer.cs
@@ -1,10 +1,17 @@
 ﻿using Ahedfi.Component.Core.Domain.Models.Entities;
 using Ahedfi.Component.Core.Domain.Models.Interfaces;
+using System.Collections.Generic;
 
 namespace Ahedfi.Server.Core.Domain.Entities
 {
     public class Customer : AuditableEntity<int>, IAggregateRoot
     {
+        public Customer()
+        {
+            Addresses = new HashSet<Address>();
+        }
         public string Name { get; set; }
+
+        public ICollection<Address> Addresses { get; set; }
     }
 }
