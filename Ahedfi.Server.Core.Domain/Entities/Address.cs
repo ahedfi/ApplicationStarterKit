f9b60ef91e5e8b6ff07052267b0f567e60e4037a
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2020-02-22 00:09:48 +0100

    Override Equals and GetHashCode

--- Ahedfi.Server.Core.Domain/Entities/Address.cs
+++ Ahedfi.Server.Core.Domain/Entities/Address.cs
@@ -1,8 +1,10 @@
 ﻿using Ahedfi.Component.Core.Domain.Models.Entities;
+using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Ahedfi.Server.Core.Domain.Entities
 {
-    public class Address: AuditableEntity<int>
+    public class Address: AuditableEntity<int>, IEquatable<Address>
     {
         public string Street { get; set; }
         public string City { get; set; }
@@ -10,5 +12,24 @@ namespace Ahedfi.Server.Core.Domain.Entities
         public string Country { get; set; }
         public string ZipCode { get; set; }
 
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Address);
+        }
+
+        public bool Equals([AllowNull] Address other)
+        {
+            return other != null &&
+                   Street == other.Street &&
+                   City == other.City &&
+                   State == other.State &&
+                   Country == other.Country &&
+                   ZipCode == other.ZipCode;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Street, City, State, Country, ZipCode);
+        }
     }
 }
--- Ahedfi.Server.Core.Domain/Entities/Customer.cs
+++ Ahedfi.Server.Core.Domain/Entities/Customer.cs
@@ -1,10 +1,12 @@
 ﻿using Ahedfi.Component.Core.Domain.Models.Entities;
 using Ahedfi.Component.Core.Domain.Models.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Ahedfi.Server.Core.Domain.Entities
 {
-    public class Customer : AuditableEntity<int>, IAggregateRoot
+    public class Customer : AuditableEntity<int>, IAggregateRoot, IEquatable<Customer>
     {
         public Customer()
         {
@@ -15,5 +17,23 @@ namespace Ahedfi.Server.Core.Domain.Entities
         public Address SecondAddress{ get; set; }
 
         public ICollection<Address> Addresses { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Customer);
+        }
+
+        public bool Equals([AllowNull] Customer other)
+        {
+            return other != null &&
+                   Name == other.Name &&
+                   EqualityComparer<Address>.Default.Equals(SecondAddress, other.SecondAddress) &&
+                   EqualityComparer<ICollection<Address>>.Default.Equals(Addresses, other.Addresses);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, SecondAddress, Addresses);
+        }
     }
 }
